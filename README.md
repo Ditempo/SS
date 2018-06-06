60b17c877b24b2f78521b00f9ae5dd5529b62290
Ditempo <gsw2205@gmail.com>
2018-06-07 08:52:37 +0900

    dd

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# SS - 웹 정적 생성기 Static Site Genertor
-SS는 Simple Static site generator 간단한 웹 정적 생성기 입니다.  
-github.io 블로그를 만들기 위해서 개발했습니다.    
-&  한글로 된 웹 정적 생성기 가이드가 없더라고요 솔직히 그래서 만들었습니다.    
+# SS - 웹 정적 생성기 Static Site Genertor   
+## 개발배경
+SS는 Simple Static site generator 간단한 웹 정적 생성기 입니다.   
+github.io 블로그를 만들기 위해서 개발했습니다.   
+// 이미 있는 것을 사용하려고 했는데 한글가이드는 없더라고요 그래서 한번 만들어 보았습니다.
+
+---
+
+##구조
--- site.config.js
+++ site.config.js
@@ -3,7 +3,8 @@ module.exports={
         title: '길상우',
         description: 'Static site generator',
         basePath: process.env.NODE_ENV === 'production' ? '/static-web-generator' : '',
-        githubPath: 'https://github.com/Ditempo/'
+        githubPath: 'https://github.com/Ditempo/',
+        aboutPath: '/about'
     },
     build: {
         outputPath: process.env.NODE_ENV === 'production' ? './docs' : './public'
